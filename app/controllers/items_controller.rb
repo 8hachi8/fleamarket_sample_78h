3dbb58ca3a32ed90e92e56a800e4d6fa9d957f3c
oniyaku <nuda.veritas.1899@gmail.com>
2020-07-14 14:06:46 +0900

    Partial_add_functions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_category
+  before_action :set_item, only: [:edit, :update]
   
   def index
   end
@@ -13,7 +14,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to user_path
     else
       flash.now[:alert] = '必須項目の内容を確認してください。'
       render :new
@@ -22,10 +23,28 @@ class ItemsController < ApplicationController
 
   def show
   end
+  
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to user_path
+    else
+    flash.now[:alert] = '必須項目の内容を確認してください。'
+    render 'edit'
+    end
+  end
+
+  
 
   private
   def set_category
     @parents = Category.where(ancestry: nil).order("id ASC")
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def item_params
